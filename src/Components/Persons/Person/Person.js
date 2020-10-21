93f200461ecfa7850fcc6a29af07d8ab31a6eb41
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-20 22:41:21 -0700

    component lifecycle prop updates

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Person.module.css";
 
-const Person = ({ name, age, children, deleteName, nameChange }) => {
-  console.log("[Person.js] rendering...");
-  return (
-    <div className={classes.Person}>
-      <p onClick={deleteName}>
-        My name is {name} and I am {age} years old.
-      </p>
-      <p>{children}</p>
-      <input type="text" onChange={nameChange} value={name} />
-    </div>
-  );
-};
+class Person extends Component {
+  render() {
+    let { name, age, children, deleteName, nameChange } = this.props;
+    console.log("[Person.js] rendering...");
+    return (
+      <div className={classes.Person}>
+        <p onClick={deleteName}>
+          My name is {name} and I am {age} years old.
+        </p>
+        <p>{children}</p>
+        <input type="text" onChange={nameChange} value={name} />
+      </div>
+    );
+  }
+}
 
 export default Person;
--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const Persons = ({ persons, deleteName, nameChange }) => {
-  console.log("[Persons.js] rendering...");
-  return persons.map((person) => {
-    return <Person key={person.id} name={person.name} age={person.age} deleteName={() => deleteName(person.id)} nameChange={(e) => nameChange(e, person.id)} />;
-  });
-};
+class Persons extends Component {
+  static getDerivedStateFromProps(props, state) {
+    console.log("[Persons.js] getDerivedStateFromProps", props, "test", state);
+    return state;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] getSnapshoBeforeUpdate");
+    return { message: "Snapshot" };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Persons.js] componentDidUpdate");
+    console.log(snapshot);
+  }
+  render() {
+    let { persons, deleteName, nameChange } = this.props;
+    console.log("[Persons.js] rendering...");
+    return persons.map((person) => {
+      return <Person key={person.id} name={person.name} age={person.age} deleteName={() => deleteName(person.id)} nameChange={(e) => nameChange(e, person.id)} />;
+    });
+  }
+}
 
 export default Persons;
